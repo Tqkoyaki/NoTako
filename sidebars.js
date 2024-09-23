d7f173c4a3234162c0217f8ff6e518b0fcedb092
Nikhil Pandey <np768@scarletmail.rutgers.edu>
2024-09-23 14:09:29 -0400

    Complex Numbers

--- sidebars.js
+++ sidebars.js
@@ -133,6 +133,7 @@ const sidebars = {
             'euler-tako/algebra-trig/equations/page-1',
             'euler-tako/algebra-trig/equations/page-2',
             'euler-tako/algebra-trig/equations/page-3',
+            'euler-tako/algebra-trig/equations/page-4',
           ]
         }
       ]
